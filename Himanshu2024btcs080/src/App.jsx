34a2dad1aa5b00ab3b934f56eae3a5c210aba867
himanshukumarsaw <sawmanojkumar65@gmail.com>
2026-07-27 14:48:15 +0530

    2:50

--- Himanshu2024btcs080/src/App.jsx
+++ Himanshu2024btcs080/src/App.jsx
@@ -1,11 +1,17 @@
 import { useState } from 'react'
 import './App.css'
 import {Routes,Route} from "react-router"
+import Home from "./features/Home/Home.jsx"
+import About from "./features/About/About.jsx"
+import Contact from "./features/Contact/Contact.jsx"
 function App() {
   return (
     <>
+    <Navbar/>
       <Routes>
-        <Route path='/' element={<Home/>}/>
+        <Route path='/home' element={<Home/>}/>
+        <Route path='/about' element={<About/>}/>
+        <Route path='/contact' element={<Contact/>}/>
       </Routes>
     </>
   )
