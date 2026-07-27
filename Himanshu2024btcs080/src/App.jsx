0abedd83a12a1fd7be6520414d55e23c87490451
himanshukumarsaw <sawmanojkumar65@gmail.com>
2026-07-27 15:01:43 +0530

    3:00

--- Himanshu2024btcs080/src/App.jsx
+++ Himanshu2024btcs080/src/App.jsx
@@ -4,12 +4,13 @@ import {Routes,Route} from "react-router"
 import Home from "./features/Home/Home.jsx"
 import About from "./features/About/About.jsx"
 import Contact from "./features/Contact/Contact.jsx"
+import Navbar from './features/shared/Navabar.jsx'
 function App() {
   return (
     <>
     <Navbar/>
       <Routes>
-        <Route path='/home' element={<Home/>}/>
+        <Route path='/' element={<Home/>}/>
         <Route path='/about' element={<About/>}/>
         <Route path='/contact' element={<Contact/>}/>
       </Routes>
---
+++ Himanshu2024btcs080/src/features/shared/Navabar.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import {Link} from "react-router"
+import "./navbar.css"
+const Navbar = () => {
+  return (
+        <div className="header">
+         <div>
+            <Link className='link' to={"/"}><h2>Logo</h2></Link>
+         </div>
+         <div>  
+            <ul>
+                <li>
+                    <Link className='link' to="/">Home</Link>
+                </li>
+                <li>
+                    <Link className='link' to="/about">About</Link>
+                </li>
+                <li>
+                    <Link className='link' to="/login">Login</Link>
+                </li>
+            </ul>
+         </div>
+    </div>
+  )
+}
+
+export default Navbar
